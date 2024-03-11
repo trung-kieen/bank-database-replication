efe1e15acb150a5eb0d7d29f8d3436b0c27346a5
kien <trungkiennk4869@gmail.com>
2024-03-11 09:16:55 +0700

    Remove: unwanted query script

--- SqlScript/4.2 Role_ChiNhanh.sql
+++
@@ -1,8 +0,0 @@
-use [NGANHANG]
-GO 
--- Run this query in server 1 and server 2
--- If already have this role use ALTER instead of CREATE
-CREATE ROLE ChiNhanh;
-GO 
-ALTER ROLE  db_owner ADD MEMBER CHINHANH;
-GO
